68bf939ee9533ba86a72213b8942b7d90339bd39
Sandro Oliveira <sandroaugusto67@gmail.com>
2022-07-26 00:04:54 -0300

    Update README.md

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Repo to add exercises on learning RUST language &lt;3
 
 # Projects
-Hello: Hellow World with variables types
+var_types: Hellow World with variables types
 
 # How to run
 cd <project-folder>
---
+++ var_types/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+   
+    // Unsigned Integer
+    // u8, u16, u32, u64, u128
+    let unsigned: u8 = 10;
+
+    // Signed Integer
+    // i8, i16, i32, i64, i128
+    let signed: i8 = -100;
+
+    // Float is used for decimals
+    let float: f32 = 1.0;
+
+    let letter = "c";
+    let emogi = "\u{1F600}";
+
+
+    println!("Helo World: Unsign: {}, signed: {}, float: {}, leter: {}, emogi: {}", unsigned, signed, float, letter, emogi);
+}
